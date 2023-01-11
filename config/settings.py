c02dc88a7d767e532c12498021c22f414d0ef50e
fo4dog <7249901@gmail.com>
2023-01-11 19:28:20 +0300

    lesson_3 Urls in templates

--- config/settings.py
+++ config/settings.py
@@ -65,11 +65,11 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "mainapp.context_processors.example.simple_context_processor",
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = "config.wsgi.application"
 
 
